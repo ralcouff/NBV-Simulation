5ca9b076806f1f382b59a4e91e50553f50876d33
Alcouffe Rémy <remy.alcouffe@toulouse-inp.fr>
2022-11-18 17:44:54 +0100

    Documentation

--- nbv_simulation/MCMF.h
+++ nbv_simulation/MCMF.h
@@ -3,9 +3,9 @@
 
 #pragma once
 
-#include <vector>
 #include <cmath>
 #include <queue>
+#include <vector>
 
 class MCMF {
 
@@ -26,14 +26,28 @@ public:
     std::vector<double> d;
     double eps = 1e-3;
 
+    /**
+     * Test if a value is Zero or not
+     * @param val The value to test
+     * @return TRUE if val < EPS, else FALSE
+     */
     [[nodiscard]] bool isZero(const double &val) const;
 
+    /**
+     * Initialize the MF solver
+     * @param _n The number of element in the Graph
+     */
     void init(int _n);
 
     void AddEdge(int from, int to, int cap, double cost);
 
     bool BellmanFord(int s, int t, int &flow, double &cost);
 
+    /**
+     * Solve the MCMF
+     * @param vec The bipartite list
+     * @return A list of views satisfying the set covering problem by MF.
+     */
     std::vector<int> work(const std::vector<std::vector<std::pair<int, double>>> &vec);
 
 };
