daf585f3e5f057553669bd8f882eac43da9db05f
Alcouffe Rémy <remy.alcouffe@toulouse-inp.fr>
2023-03-27 17:04:46 +0200

    REformatting the initialization, adding colors to poor quality points.

--- nbv_simulation/Share_Data.cpp
+++ nbv_simulation/Share_Data.cpp
@@ -2,20 +2,22 @@
 
 Share_Data::Share_Data(std::string _config_file_path) {
     process_cnt = -1;
-    yaml_file_path = std::move(_config_file_path);
-    cout << yaml_file_path << endl;
+
     // Reading yaml files
+    yamlConfigFilePath = std::move(_config_file_path);
+    cout << "---------------------------------------------------------" << endl;
+    cout << "Reading the configuration file: " << yamlConfigFilePath << endl;
     cv::FileStorage fs;
-    fs.open(yaml_file_path, cv::FileStorage::READ);
-    fs["model_path"] >> pcd_file_path;
-    fs["name_of_pcd"] >> name_of_pcd;
+    fs.open(yamlConfigFilePath, cv::FileStorage::READ);
+    fs["model_path"] >> objectFolderPath;
+    fs["name_of_pcd"] >> nameOfObject;
     fs["method_of_IG"] >> method_of_IG;
     fs["octomap_resolution"] >> octomap_resolution;
     fs["ground_truth_resolution"] >> ground_truth_resolution;
     fs["num_of_max_iteration"] >> num_of_max_iteration;
     fs["show"] >> show;
     fs["move_wait"] >> move_wait;
-    fs["nbv_net_path"] >> nbv_net_path;
+    fs["nbv_net_path"] >> nbvNetPath;
     fs["p_unknown_upper_bound"] >> p_unknown_upper_bound;
     fs["p_unknown_lower_bound"] >> p_unknown_lower_bound;
     fs["num_of_views"] >> num_of_views;
@@ -38,6 +40,15 @@ Share_Data::Share_Data(std::string _config_file_path) {
     fs["color_p2"] >> color_intrinsics.coeffs[4];
     fs["depth_scale"] >> depth_scale;
     fs.release();
+
+    // Building the different file paths
+    objectFilePath = objectFolderPath + nameOfObject;
+    qualityFilePath = objectFilePath + ".qlt";
+    viewSpaceFilePath = objectFilePath + ".txt";
+    savePath = "../results/" + nameOfObject + '_' + std::to_string(method_of_IG);
+    if (method_of_IG == 0)
+        savePath += '_' + std::to_string(cost_weight);
+
     /* Populating the SfM_Data from AliceVision */
     sfm_data.getIntrinsics().emplace(0, std::make_shared<aliceVision::camera::Pinhole>(color_intrinsics.width,
                                                                                        color_intrinsics.height,
@@ -56,9 +67,15 @@ Share_Data::Share_Data(std::string _config_file_path) {
     // Read the pcd file of the converted model
     pcl::PointCloud<pcl::PointXYZ>::Ptr temp_pcd(new pcl::PointCloud<pcl::PointXYZ>);
     cloud_pcd = temp_pcd;
-    cout << pcd_file_path + name_of_pcd + ".pcd" << endl;
-    if (pcl::io::loadPCDFile<pcl::PointXYZ>(pcd_file_path + name_of_pcd + ".pcd", *cloud_pcd) == -1) {
-        cout << "Can not read 3d model file. Check." << endl;
+    if (pcl::io::loadPCDFile<pcl::PointXYZ>(objectFilePath + ".pcd", *cloud_pcd) == -1) {
+        cout << "3D PCD File not found. Trying to open OBJ file." << endl;
+        if (pcl::io::loadOBJFile<pcl::PointXYZ>(objectFilePath + ".obj", *cloud_pcd) == -1) {
+            cout << "3D OBJ File not found. Trying to open PLY file." << endl;
+            if (pcl::io::loadPLYFile<pcl::PointXYZ>(objectFilePath + ".ply", *cloud_pcd) == -1) {
+                cout << "3D PLY File not found." << endl;
+                cout << "No 3D file where found there. Check your input name : " << objectFilePath << endl;
+            }
+        }
     }
 /*    *//* Add the initial PC to the sfm_data pipeline. *//*
     float i = 0;
@@ -69,24 +86,30 @@ Share_Data::Share_Data(std::string _config_file_path) {
     aliceVision::sfmDataIO::saveJSON(sfm_data, "tartuffe.sfm", aliceVision::sfmDataIO::ESfMData::ALL);
     cout << "pcl_loaded" << endl;*/
 //    sfm_data.getLandmarks().emplace(0, aliceVision::sfmData::Landmark() {x, y, z})
+
+    // Generating the octomaps
     octo_model = new octomap::ColorOcTree(octomap_resolution);
     ground_truth_model = new octomap::ColorOcTree(ground_truth_resolution);
     GT_sample = new octomap::ColorOcTree(octomap_resolution);
+
+    // Initializing the camera pose world to identity
+    now_camera_pose_world = Eigen::Matrix4d::Identity(4, 4);
+
+    // Initializing parameters
     if (num_of_max_flow_node == -1)
         num_of_max_flow_node = num_of_views;
-    now_camera_pose_world = Eigen::Matrix4d::Identity(4, 4);
     over = false;
     pre_clock = (double) clock();
     valid_clouds = 0;
+
+    // Initializing the Point Clouds
     pcl::PointCloud<pcl::PointXYZRGB>::Ptr temp(new pcl::PointCloud<pcl::PointXYZRGB>);
     cloud_final = temp;
     pcl::PointCloud<pcl::PointXYZRGB>::Ptr temp_gt(new pcl::PointCloud<pcl::PointXYZRGB>);
     cloud_ground_truth = temp_gt;
-    save_path = "../" + name_of_pcd + '_' + std::to_string(method_of_IG);
-    if (method_of_IG == 0)
-        save_path += '_' + std::to_string(cost_weight);
-    cout << "pcd and yaml files read." << endl;
-    cout << "save_path is: " << save_path << endl;
+
+    cout << "3D object and YAML files have been read." << endl;
+    cout << "Saving results in: " << savePath << endl;
     srand(clock());
 }
 
@@ -151,10 +174,10 @@ void Share_Data::save_cloud_to_disk(const pcl::PointCloud<pcl::PointXYZRGB>::Ptr
                                     const std::string &name) const {
     std::stringstream cloud_stream, path_stream;
     std::string cloud_file, path;
-    path_stream << save_path << cd;
+    path_stream << savePath << cd;
     path_stream >> path;
     access_directory(path);
-    cloud_stream << save_path << cd << "/" << name << ".pcd";
+    cloud_stream << savePath << cd << "/" << name << ".pcd";
     cloud_stream >> cloud_file;
     pcl::io::savePCDFile<pcl::PointXYZRGB>(cloud_file, *cloud);
 }
--- nbv_simulation/Share_Data.h
+++ nbv_simulation/Share_Data.h
@@ -23,6 +23,7 @@
 #include <pcl/filters/passthrough.h>
 #include <pcl/io/pcd_io.h>
 #include <pcl/io/ply_io.h>
+#include <pcl/io/obj_io.h>
 #include <pcl/point_types.h>
 #include <pcl/registration/icp.h>
 #include <pcl/visualization/cloud_viewer.h>
@@ -52,95 +53,92 @@ using Views = aliceVision::HashMap<aliceVision::IndexT, std::shared_ptr<View> >;
 #define NBVNET 6
 #define PCNBV 7
 #define NewOurs 8
+#define Test_one 101
 
 /**
  * The class that contains all the data needed for the NBV algorithm.
  */
 class Share_Data {
 public:
+
     // Variable input parameters
-    std::string pcd_file_path;
-    std::string yaml_file_path;
-    std::string name_of_pcd;
-    std::string nbv_net_path;
-
-    // Number of viewpoints sampled at one time
-    int num_of_views{};
-    double cost_weight{};
-    rs2_intrinsics color_intrinsics{};
-    double depth_scale{};
-
-    // Operating parameters
-    // Process number
-    int process_cnt;
-    // System clock
-    std::atomic<double> pre_clock{};
-    // Is the process finished
-    std::atomic<bool> over{};
-    bool show{};
-    int num_of_max_iteration{};
+    std::string yamlConfigFilePath;
+    std::string objectFolderPath;
+    std::string nameOfObject;
+    std::string nbvNetPath;
+    std::string objectFilePath;
+    std::string viewSpaceFilePath;
+    std::string qualityFilePath;
+    std::string savePath;
 
     // Point cloud data
     std::atomic<int> valid_clouds{};
-    // Point cloud group
-    std::vector<pcl::PointCloud<pcl::PointXYZRGB>::Ptr> clouds;
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_pcd;
-    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_ground_truth;
-    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_final;
-    bool move_wait{};
+    std::vector<pcl::PointCloud<pcl::PointXYZRGB>::Ptr> clouds; // A group containing all the clouds
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_pcd; // The input cloud from the reconstruction algorithm
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_ground_truth; // The Ground Truth cloud containing the rescaled input cloud
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_final; // The final cloud, output of the algorithm
+    int init_voxels{}; // Number of point cloud voxels
 
-    // Eight Forks Map
-    octomap::ColorOcTree *octo_model;
-    // octomap::ColorOcTree* cloud_model;
-    octomap::ColorOcTree *ground_truth_model;
-    octomap::ColorOcTree *GT_sample;
-    double octomap_resolution{};
-    double ground_truth_resolution{};
-    double map_size{};
+    // Octomaps
+    octomap::ColorOcTree *octo_model; //TODO
+    // octomap::ColorOcTree* cloud_model; //TODO
+    octomap::ColorOcTree *ground_truth_model; //TODO
+    octomap::ColorOcTree *GT_sample; //TODO
+    double octomap_resolution{}; // The resolution of the current octomap model
+    double ground_truth_resolution{}; // The resolution of the Ground Truth octomap
     double p_unknown_upper_bound{}; //! Upper bound for voxels to still be considered uncertain. Default: 0.97.
     double p_unknown_lower_bound{}; //! Lower bound for voxels to still be considered uncertain. Default: 0.12.
+    int voxels_in_BBX{}; // Number of voxels in the map
+    double init_entropy{}; // Map information entropy
 
-    // Workspace and viewpoint space
-    std::atomic<bool> now_view_space_processed{};
-    std::atomic<bool> now_views_information_processed{};
-    std::atomic<bool> move_on{};
+    // Process flags and parameters
+    std::atomic<bool> over{}; // Checks if the process is over
+    std::atomic<bool> now_view_space_processed{}; // Is the View Space processed
+    std::atomic<bool> now_views_information_processed{};  // Are the Views Information processed
+    std::atomic<bool> move_on{}; // Did we continue
+    int process_cnt; // Process number
+    std::atomic<double> pre_clock{}; // System clock
 
-    Eigen::Matrix4d now_camera_pose_world;
-    // Object centre
-    Eigen::Vector3d object_center_world;
-    // Object BBX radius
-    double predicted_size{};
+    // Quality set
+    std::unordered_map<octomap::OcTreeKey, double, octomap::OcTreeKey::KeyHash> *quality_weight; // A quality map for each voxel
 
-    int method_of_IG{};
+    // Position matrices
+    Eigen::Matrix4d now_camera_pose_world; // The current camera pose considered as the NBV
+    Eigen::Vector3d object_center_world; // The barycenter of the object, used as the center of the world
+    double predicted_size{}; // The estimated radius of the object
+    double map_size{}; // The radius of the octomap
+
+    //Pipeline parameters
+    int method_of_IG; // Method of Information Gain computation
+    int num_of_max_iteration; // Number of maximum iteration
+    int num_of_views; // Number of views sampled around the object
+    double cost_weight{}; // Gamma in Information Gain computation
+    double skip_coefficient{}; // A coefficient to skip some pixels in the projected image
+    double sum_local_information{}; // The sum of all local information
+    double sum_global_information{}; // The sum of all global information
+    double sum_robot_cost{}; // The sum of robot cost
+    std::vector<View> best_views{}; // A list containing the successive best views
+    bool show{}; // To show the interface or not
+    bool move_wait{}; //TODO
+    bool robot_cost_negative{}; //TODO
+    int num_of_max_flow_node; // The maximum number of nodes for the max flow algorithm.
+    double interesting_threshold{}; // The threshold to determine if it's interesting or not //TODO
+
+
+    // Camera parameters
+    rs2_intrinsics color_intrinsics{};
+    double depth_scale{};
+
+    // A visualizer for the octomaps and clouds
     pcl::visualization::PCLVisualizer::Ptr viewer;
 
+    // An sfm Data to store the results
+    aliceVision::sfmData::SfMData sfm_data{};
+
+    // Unused variables
     double stop_thresh_map{};
     double stop_thresh_view{};
-
-    double skip_coefficient{};
-
-    double sum_local_information{};
-    double sum_global_information{};
-
-    double sum_robot_cost{};
     double camera_to_object_dis{};
-    bool robot_cost_negative{};
-
-    int num_of_max_flow_node;
-    double interesting_threshold{};
-
-    // Number of point cloud voxels
-    int init_voxels{};
-    // Number of voxels on the map
-    int voxels_in_BBX{};
-    // Map information entropy
-    double init_entropy{};
-
-    std::string save_path;
-
-    std::vector<View> best_views{};
-
-    aliceVision::sfmData::SfMData sfm_data{};
 
     /**
      * Constructor of the Share_Data object
