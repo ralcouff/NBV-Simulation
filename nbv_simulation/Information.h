5469fa0e80539d11dd01c6989df0de7fa651edda
Alcouffe Rémy <remy.alcouffe@toulouse-inp.fr>
2023-07-04 15:35:22 +0200

    Working ray_caster with modified Ray class

--- nbv_simulation/Information.h
+++ nbv_simulation/Information.h
@@ -136,8 +136,5 @@ void information_gain_thread_process(Ray_Information **rays_info,
                                      View_Space *view_space,
                                      int pos);
 
-//bool castRay(const octomap::ColorOcTree *octo_model, const octomap::point3d& origin, const octomap::point3d& directionP, octomap::point3d& end,
-//             bool ignoreUnknown, double maxRange);
-
 bool castRay(const octomap::ColorOcTree *octo_model, const octomap::point3d& origin, const octomap::point3d& directionP,
              octomap::point3d& end_unknown, bool ignoreUnknown, double maxRange, double p_unknown_upper_bound);
